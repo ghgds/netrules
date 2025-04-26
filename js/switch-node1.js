d7717a083b44c304ebde48177517e6f27cff7097
Gds <31283292+ghgds@users.noreply.github.com>
2025-04-26 21:35:31 +0800

    Create switch-node1.js

---
+++ js/switch-node1.js
@@ -0,0 +1,55 @@
+const groupName = 'Loadb'; // 策略组名
+
+(async () => {
+  const now = new Date();
+  const hour = now.getHours();
+  const day = now.getDay(); // 0=周日
+
+  if (hour < 18) {
+    $done();
+    return;
+  }
+  if (day > 0 & day < 5) {
+    $done();
+    return;
+  }
+
+  $httpAPI("GET", "/v1/policy_groups", null, (data) => {
+    try {
+      const group = data[groupName];
+      if (!group || !Array.isArray(group) || group.length === 0) {
+        $done();
+        return;
+      }
+      // 只提取节点名
+      const allPolicies = group.filter(p => !p.isGroup).map(p => p.name);
+      if (allPolicies.length === 0) {
+        $done();
+        return;
+      }
+      // 第二步：拿到当前已选节点
+      $httpAPI("GET", `/v1/policy_groups/select?group_name=${encodeURIComponent(groupName)}`, null, (selectedData) => {
+        try {
+          const currentPolicy = selectedData.policy;
+          const currentIndex = allPolicies.indexOf(currentPolicy);
+          let nextIndex = (currentIndex + 1) % allPolicies.length;
+          const nextPolicy = allPolicies[nextIndex];
+          if (!nextPolicy) {
+            $done();
+            return;
+          }
+          $httpAPI("POST", "/v1/policy_groups/select", { group_name: groupName, policy: nextPolicy }, (res) => {
+            //$notification.post('Surge节点顺序切换成功', groupName, `切换到 ${nextPolicy}`);
+            $done();
+          });
+        } catch (e) {
+          console.log('切换节点失败:', e);
+          $done();
+        }
+      });
+    } catch (e) {
+      console.log('脚本运行错误:', e);
+      $done();
+    }
+  });
+})();
